211973f0737619e8c848077f5137ea488a476edd
João Victor Ignacio <joao.ignacio@venturus.org.br>
2021-06-07 18:20:07 -0300

    Setup

--- docker-run-cmd.sh
+++ docker-run-cmd.sh
@@ -4,7 +4,7 @@
 
 # Macbook
 
-# docker run -d --name afrodev-db -p 3306:3306 -v /${pwd}/docker/docker-entrypoint-initdb.d:/docker-entrypoint-initdb.d -e MYSQL_ROOT_PASSWORD=mysql-12345 mysql:5.7
+# docker run -d --name womencancode-db -p 3306:3306 -v /${pwd}/docker/docker-entrypoint-initdb.d:/docker-entrypoint-initdb.d -e MYSQL_ROOT_PASSWORD=mysql-12345 mysql:5.7
 
 # Windows + WSL
 # Verify EOL! Must be LF and not CRLF
