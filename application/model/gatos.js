fcab2d251009b998665b54e6c245a4b42512a6c3
celiareginar <59150615+celiareginar@users.noreply.github.com>
2021-06-08 21:10:39 -0300

    Classe Gatos

---
+++ application/model/gatos.js
@@ -0,0 +1,47 @@
+const { DataTypes } = require('sequelize');
+
+const Model = require('./baseModel');
+
+class Gatos extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          allowNull: false,
+          primaryKey: true,
+        },
+        tipo: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        possuiDeficiencia: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        cuidadosEspeciais: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        corPelagem: DataTypes.STRING,
+        sexo: DataTypes.STRING,
+        idade: DataTypes.STRING,
+        castrado: DataTypes.STRING,
+        teste: DataTypes.STRING,
+        vacinas: DataTypes.STRING,
+      },
+      {
+        sequelize,
+        underscored: true,
+        tableName: 'gatos',
+      },
+    );
+  }
+}
+
+module.exports = Gatos;
