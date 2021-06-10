301609348c0091d16608691662060fdf695f4f96
Juliana Mesquita <60270373+JulianaMariaSousaMesquita@users.noreply.github.com>
2021-06-09 21:08:49 -0300

    Movendo codigo

--- adapters/api/routes/ong.js
+++ adapters/api/routes/ong.js
@@ -1,31 +1,5 @@
 const controller = require('../../controllers/ong');
 const validators = require('../validators/ong');
-const multer = require('multer');
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb){
-    cb(null, './uploads/');
-  },
-  filename: function(req, file, cb){
-    cb(null, file.originalname); //new Date().toISOString() +
-  }
-});
-
-const fileFilter = (req, file, cb) => {
-  if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg'){
-    cb(null, true);
-  }else{
-    cb(null,false);
-  }
-}
-
-const upload = multer ({
-  storage : storage,
-  limits : {
-    fileSize : 1024 * 1024 * 3
-  },
-  fileFilter : fileFilter
-});
 
 const invalidRequfestReply = (request, reply, errors) => reply.status(400).json({
   method: request.method,
@@ -86,10 +60,5 @@ module.exports = (app) => {
     const response = await controller.delete(request.params.id, request, reply);
     return reply.json(response);
   });
-
-  app.post('/upload', upload.single('cat_image'), function (req, res, next) {
-    const response = req.file;
-    return  res.status(200).send(response);
-  });
-
+  
 };
---
+++ adapters/api/routes/upload.js
@@ -0,0 +1,40 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb){
+    cb(null, './uploads/');
+  },
+  filename: function(req, file, cb){
+    cb(null, file.originalname); //new Date().toISOString() +
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg'){
+    cb(null, true);
+  }else{
+    cb(null,false);
+  }
+}
+
+const upload = multer ({
+  storage : storage,
+  limits : {
+    fileSize : 1024 * 1024 * 3
+  },
+  fileFilter : fileFilter
+});
+
+const invalidRequfestReply = (request, reply, errors) => reply.status(400).json({
+  method: request.method,
+  status: reply.statusCode,
+  error: errors,
+});
+
+module.exports = (app) => {   
+  app.post('/upload', upload.single('cat_image'), function (req, res, next) {
+    const response = req.file;
+    return  res.status(200).send(response);
+  });
+
+};
