240c9650efbc20c03c959162b42f4b356b92cde3
Graciliana <eagraciliana@gmail.com>
2021-06-10 20:58:06 -0300

    solucionado a remoção dos console.log

--- application/services/gatos.js
+++ application/services/gatos.js
@@ -1,12 +1,10 @@
 const Gato = require('../model/gatos')
 
 exports.register = async (gatos) =>{
-    try{
-        console.log(gatos)
+    try{ 
         const newGato = await Gato.create(gatos);
         return newGato;
     }catch (erro){
-        console.log(erro)
         const error = new Error("Deu ruim na criacao")
         throw error;
     }
