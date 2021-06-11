6302b3785978fec7b58072de019dfea1617021e3
Juliana Mesquita <60270373+JulianaMariaSousaMesquita@users.noreply.github.com>
2021-06-10 21:39:05 -0300

    Adicionando campo cat_image em create-gatos.js

--- database/migrations/create-gatos.js
+++ database/migrations/create-gatos.js
@@ -1,64 +1,68 @@
 module.exports = {
-    up: async (queryInterface, Sequelize) => queryInterface.createTable('gatos', {
-        id: {
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        tipo: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        status: {
-            type: Sequelize.STRING,
-            allowNull: true,
-        },
-        name: {
-            type: Sequelize.STRING,
-            allowNull: true,
-        },
-        possuiDeficiencia: {
-            type: Sequelize.STRING,
-            allowNull: true,
-        },
-        cuidadosEspeciais: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        corPelagem: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        sexo: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        idade: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        castrado: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        teste: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        vacinas: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        created_at: {
-            type: Sequelize.DATE,
-            allowNull: false,
-        },
-        updated_at: {
-            type: Sequelize.DATE,
-            allowNull: false,
-        },
+  up: async (queryInterface, Sequelize) =>
+    queryInterface.createTable('gatos', {
+      cat_image: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      tipo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      possuiDeficiencia: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      cuidadosEspeciais: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      corPelagem: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      sexo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      idade: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      castrado: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      teste: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      vacinas: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     }),
 
-
-    down: async (queryInterface) => queryInterface.dropTable('gatos'),
+  down: async (queryInterface) => queryInterface.dropTable('gatos'),
 };
